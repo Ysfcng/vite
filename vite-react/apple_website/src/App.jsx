31f3f8006483f4ff95bb32512b1e7cf1e86c3000
yusuf <yc166959@gmail.com>
2024-11-19 01:43:35 -0500

    first commit

--- vite-react/apple_website/src/App.jsx
+++ vite-react/apple_website/src/App.jsx
@@ -1,19 +1,26 @@
 import Navbar from "/src/components/Navbar.jsx"
 import Hero from "/src/components/Hero.jsx"
 import HighLights from "/src/components/HightLights.jsx"
-
+import Model from "/src/components/Model.jsx"
+import * as Sentry from "@sentry/react"
+import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
 const App=()=> {
   
-
   return (
     <>
     <main>
       <Navbar/>
       <Hero/>
       <HighLights />
+      <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
     </>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
